37ec4ad59bd916f73a9cee7e45278e4cfb535120
Leo Tiez <leozeitler95@gmail.com>
2020-01-31 12:53:30 +0100

    Update: Tried to find issue with the stimulus reconstruction.
    Could not yet find the issue

--- CSCodingNetworkBarranca.py
+++ CSCodingNetworkBarranca.py
@@ -9,6 +9,10 @@ import matplotlib.pyplot as plt
 from collections import Counter
 import nest
 
+nest.Install("nestmlmodule")
+
+VERBOSITY = 2
+
 
 def load_image(name, path=None):
     if path is None:
@@ -29,7 +33,7 @@ def observations_from_linear_model(
         threshold_pot=1.,
         rest_pot=0.,
 ):
-    return (float(time_const / 1000.) * firing_rates + 0.5) * (threshold_pot - rest_pot) -\
+    return ((time_const / 1000.) * firing_rates + 0.5) * (threshold_pot - rest_pot) -\
            (connection_strength * sensor_connect_mat.dot(firing_rates))  # Divide time constant by 1000 to obtain secs?
 
 
@@ -44,7 +48,7 @@ def stimulus_reconstruction(
         rest_pot=0.,
         stimulus_size=1e4
 ):
-    cosine_tranform = idct(np.identity(int(np.sqrt(stimulus_size))), norm="ortho", axis=0)
+    cosine_tranform = dct(np.identity(int(np.sqrt(stimulus_size))), norm="ortho", axis=0)
     kron_cosine = np.kron(cosine_tranform, cosine_tranform)
     inv_kron_cosing = np.linalg.inv(kron_cosine)
     observations = observations_from_linear_model(
@@ -59,16 +63,14 @@ def stimulus_reconstruction(
     # The L1 optimisation
     optimisation_vector = cvx.Variable(int(stimulus_size))
     objective = cvx.Minimize(cvx.norm(optimisation_vector, 1))
-    constraints = [receptor_connection_strength * receptor_sensor_connect.T.dot(inv_kron_cosing) * optimisation_vector
-                   == observations]
+    sampling_trans_mat = receptor_connection_strength * receptor_sensor_connect.T.dot(inv_kron_cosing)
+    constraints = [sampling_trans_mat * optimisation_vector == observations]
     prob = cvx.Problem(objective, constraints)
     result = prob.solve(verbose=True)
     stimulus_pixel_matrix = np.array(optimisation_vector.value).squeeze()
     stimulus_pixel_matrix.reshape(int(np.sqrt(stimulus_size)), int(np.sqrt(stimulus_size)))
     stimulus = idct2(stimulus_pixel_matrix)
-    stimulus /= float(stimulus.max())
-    stimulus *= 255
-    plt.imshow(stimulus.reshape(int(np.sqrt(stimulus_size)), int(np.sqrt(stimulus_size))))
+    plt.imshow(stimulus.reshape(int(np.sqrt(stimulus_size)), int(np.sqrt(stimulus_size))), cmap='gray')
     plt.show()
     return stimulus
 
@@ -81,9 +83,14 @@ def create_input_current_generator(
     assert np.all(input < 256) and np.all(input >= 0)
 
     # Transforming to k-sparse stimulus
+    shape = input.shape
     trans_input = np.asarray(input).copy().reshape(-1)
     fourier_trans_input = fft(trans_input)
     trans_input[fourier_trans_input <= epsilon] = 0
+    if VERBOSITY > 1:
+        plt.imshow(trans_input.reshape(shape), cmap='gray')
+        plt.show()
+
     current_dict = [{"amplitude": float(amplitude)} for amplitude in trans_input]
 
     dc_generator = nest.Create("dc_generator", n=int(num_receptors), params=current_dict)
@@ -94,16 +101,26 @@ def create_sensory_nodes(
         num_neurons=1e3,
         time_const=20.0,
         rest_pot=0.0,
-        threshold_pot=1.0
-
+        threshold_pot=1.0,
+        capacitance=1.0
 ):
     neuron_dict = {
         "tau_m": time_const,
         "V_th": threshold_pot,
         "V_reset": rest_pot,
-        "E_L": rest_pot
+        "E_L": rest_pot,
+        "C_m":  capacitance
     }
-    sensory_nodes = nest.Create("iaf_psc_alpha", n=int(num_neurons), params=neuron_dict)
+
+    neuron_dict = {
+        "tau_m": time_const,
+        "V_th": threshold_pot,
+        "V_R": rest_pot,
+        "C_m": capacitance
+    }
+
+    # sensory_nodes = nest.Create("iaf_psc_delta", n=int(num_neurons), params=neuron_dict)
+    sensory_nodes = nest.Create("barranca_neuron", n=int(num_neurons), params=neuron_dict)
 
     # Create spike detector for stimulus reconstruction
     spikedetector = nest.Create("spike_detector", params={"withgid": True, "withtime": True})
@@ -131,19 +148,21 @@ def create_connections_random(
 
 
 def main():
-    image = load_image("sunflower.jpg")
-    plt.imshow(image)
-    plt.show()
-    simulation_time = 1000.0
+    image = load_image("sunflower50.jpg")
+    if VERBOSITY > 1:
+        plt.imshow(image, cmap='gray')
+        plt.show()
+
+    simulation_time = 250.0
     num_receptors = image.size
     num_sensors = num_receptors // 10
-    indegree_rec_sen = 10
-    indegree_sen_sen = 50
+    indegree_rec_sen = 5
+    indegree_sen_sen = 25
     cap_s = 1.
     receptor_connect_strength = 1.
     num_sensor_connections = indegree_sen_sen * num_sensors
 
-    receptor_nodes = create_input_current_generator(image, num_receptors=num_receptors)
+    receptor_nodes = create_input_current_generator(image, num_receptors=num_receptors, epsilon=0.0)
     sensory_nodes, spike_detect, multi_meter = create_sensory_nodes(num_neurons=num_sensors)
 
     create_connections_random(
@@ -172,11 +191,17 @@ def main():
 
     data_sp = nest.GetStatus(spike_detect, keys="events")[0]
     spikes_s = data_sp["senders"]
+    time_s = data_sp["times"]
+    if VERBOSITY > 1:
+        plt.plot(time_s, spikes_s, "g.")
+        plt.show()
     spike_count = Counter(spikes_s)
     firing_rates = np.zeros(len(sensory_nodes))
     for value, number in spike_count.items():
         firing_rates[int(value) - min(sensory_nodes)] = number / float(simulation_time / 1000.)
 
+    average_firing_rate = np.mean(firing_rates)
+    print("\n##################### \t Average firing rate: %s \n" % average_firing_rate)
     stimulus_reconstruction(
         firing_rates,
         cap_s/float(num_sensor_connections),
