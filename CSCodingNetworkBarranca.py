edb98153a59784c8e6619a140c7a355d99e3510a
Leo Tiez <leozeitler95@gmail.com>
2020-02-06 13:50:40 +0100

    Update: Changed file to use functions in defined in modules

--- CSCodingNetworkBarranca.py
+++ CSCodingNetworkBarranca.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 # ####################################################################################################################
 # This Python script implements the input stimulus reconstruction based on the neural response of a network of
@@ -10,197 +11,24 @@
 # https://doi.org/10.1371/journal.pcbi.1003793
 # ####################################################################################################################
 
+# Own libraries
+from modules.stimulusReconstruction import stimulus_reconstruction
+from modules.createStimulus import *
+from modules.networkConstruction import *
+from modules.networkAnalysis import *
+from modules.thesisUtils import *
+
 # External libraries
 import numpy as np
-import cvxpy as cvx
-from scipy.fft import idct
 import matplotlib.pyplot as plt
-
-# Own libraries
-from modules.thesisUtils import *
-from modules.networkAnalysis import *
 
 # Nest
 import nest
 
 # Import customised neural model
-nest.Install("nestmlmodule")
+# nest.Install("nestmlmodule")
 
 VERBOSITY = 2
-
-
-def observations_from_linear_model(
-        firing_rates,
-        sensor_connect_mat,
-        connection_strength,
-        time_const=20.0,
-        threshold_pot=1e3,
-        rest_pot=0.,
-):
-    """
-    Compute the observations from the linearised neural activity for stimulus reconstruction.
-    The computation is based on the paper by Barranca et al. (see above).
-    :param firing_rates: The firing rates of the neurons obtained from the network response
-    :param sensor_connect_mat: Connection matrix A of the sensory to sensory neurons. A_ij = 1 if there exists a
-     connection between node i and j and 0 otherwise.
-    :param connection_strength: Weight of the connections between sensory to sensory neurons. Note that this weight
-    is equal to S / N_A where S is a multiplier for the connection strength and N_A is the number of connections
-    of in the sensory connection matrix
-    :param time_const: Time constant in ms
-    :param threshold_pot: Threshold potential in mV
-    :param rest_pot: resting potential / reset potential in mV
-    :return: The observation that is used for the stimulus reconstruction
-    """
-    return ((time_const / 1000.) * firing_rates + 0.5) * (threshold_pot - rest_pot) -\
-           (connection_strength * sensor_connect_mat.dot(firing_rates))
-
-
-def stimulus_reconstruction(
-        firing_rates,
-        sensor_connection_strength,
-        receptor_connection_strength,
-        receptor_sensor_connect_mat,
-        sensor_connect_mat,
-        time_const=20.0,
-        threshold_pot=1e3,
-        rest_pot=0.,
-        stimulus_size=1e4
-):
-    """
-    Reconstruct input stimulus based on the firing patterns of the nodes in the network
-    :param firing_rates: Firing rates of the individual nodes in Hz
-    :param sensor_connection_strength: Synaptic weight of the connections of sensory-to-sensory-node links. Note that
-    this weight is equal to S / N_A where S is a multiplier for the connection strength and N_A is the number of
-    connections
-    :param receptor_connection_strength: Synaptic weight of the connections of receptor-to-sensory-node links
-    :param receptor_sensor_connect_mat: Connection matrix A for receptor-to-sensory-node links. A_ij = 1 if there exists
-     a connection between node i and j and 0 otherwise.
-    :param sensor_connect_mat: Connection matrix B for sensory-to-sensory-node links. B_ij = 1 if there exists
-     a connection between node i and j and 0 otherwise.
-    :param time_const: Membran time constant in ms
-    :param threshold_pot: Threshold potential in mV
-    :param rest_pot: resting potential in mV
-    :param stimulus_size: number of the input stimulus values
-    :return:
-    """
-    # Use that for the Kronecker product inv(A kron B) == inv(A) kron inv(B)
-    cosine_tranform = idct(np.identity(int(np.sqrt(stimulus_size))), norm="ortho", axis=0)
-    kron_cosine = np.kron(cosine_tranform, cosine_tranform)
-    # Compute transformed observations based on the linearisation of the model for the L1 optimisation procedure
-    observations = observations_from_linear_model(
-        firing_rates=firing_rates,
-        time_const=time_const,
-        threshold_pot=threshold_pot,
-        rest_pot=rest_pot,
-        sensor_connect_mat=sensor_connect_mat,
-        connection_strength=sensor_connection_strength
-    )
-
-    # The L1 optimisation
-    optimisation_vector = cvx.Variable(int(stimulus_size))
-    objective = cvx.Minimize(cvx.norm(optimisation_vector, 1))
-    sampling_trans_mat = receptor_connection_strength * receptor_sensor_connect_mat.T.dot(kron_cosine)
-    constraints = [sampling_trans_mat * optimisation_vector == observations]
-    prob = cvx.Problem(objective, constraints)
-    _ = prob.solve(verbose=True)
-    stimulus_pixel_matrix = np.array(optimisation_vector.value).squeeze()
-    stimulus_pixel_matrix = stimulus_pixel_matrix.reshape(int(np.sqrt(stimulus_size)), int(np.sqrt(stimulus_size))).T
-    stimulus = idct2(stimulus_pixel_matrix)
-
-    if VERBOSITY > 1:
-        plt.imshow(stimulus.reshape(int(np.sqrt(stimulus_size)), int(np.sqrt(stimulus_size))), cmap='gray')
-        plt.show()
-
-    return stimulus
-
-
-def create_input_current_generator(
-        input_stimulus
-):
-    """
-    Create direct current generator to simulate input stimulus. The pixel values of the image are transformed
-    to an integer value representing the intensity in Ampere A
-    :param input_stimulus: Grayscale input stimulus with integer values between 0 and 256
-    :return: Tuple with ids for the dc generator devices
-    """
-    assert np.all(input_stimulus < 256) and np.all(input_stimulus >= 0)
-
-    num_receptors = input_stimulus.size
-    # Multiply value with 1e12, as the generator expects values in pA
-    current_dict = [{"amplitude": float(amplitude * 1e12)} for amplitude in input_stimulus.reshape(-1)]
-    dc_generator = nest.Create("dc_generator", n=int(num_receptors), params=current_dict)
-    return dc_generator
-
-
-def create_sensory_nodes(
-        num_neurons=1e3,
-        time_const=20.0,
-        rest_pot=0.0,
-        threshold_pot=1e3,
-        capacitance=1e12,
-        use_barranca=True
-):
-    """
-    Create the sensory nodes of the network
-    :param num_neurons: Number of sensory nodes that have to be created
-    :param time_const: Membrane time constant in ms
-    :param rest_pot: Resting potential / reset potential in mV
-    :param threshold_pot: Threshold potential in mV
-    :param capacitance: Capacitance of the membrane in pF
-    :param use_barranca: Flag determining whether the customised Barranca neuron should be used
-    :return: Tuple with ids of the neurons
-    """
-    if use_barranca:
-        neuron_dict_barranca = {
-            "tau_m": time_const,
-            "V_th": threshold_pot,
-            "V_R": rest_pot,
-            "C_m": capacitance
-        }
-        sensory_nodes = nest.Create("barranca_neuron", n=int(num_neurons), params=neuron_dict_barranca)
-    else:
-        neuron_dict_iaf_delta = {
-            "V_m": rest_pot,
-            "E_L": rest_pot,
-            "C_m": capacitance,
-            "tau_m": time_const,
-            "V_th": threshold_pot,
-            "V_reset": rest_pot
-        }
-        sensory_nodes = nest.Create("iaf_psc_delta", n=int(num_neurons), params=neuron_dict_iaf_delta)
-
-    # Create spike detector for stimulus reconstruction
-    spikedetector = nest.Create("spike_detector", params={"withgid": True, "withtime": True})
-    multimeter = nest.Create("multimeter", params={"withtime": True, "record_from": ["V_m"]})
-    nest.Connect(sensory_nodes, spikedetector)
-    nest.Connect(multimeter, sensory_nodes)
-    return sensory_nodes, spikedetector, multimeter
-
-
-def create_connections_random(
-        src_nodes,
-        target_nodes,
-        indegree=10,
-        connection_strength=0.7,
-):
-    """
-    Create synaptic connections from source to target nodes that are not limited in the area (i.e. the function does
-    not consider receptive fields)
-    :param src_nodes: Source nodes
-    :param target_nodes: Target nodes
-    :param indegree: Number of pre-synaptic nodes per neuron
-    :param connection_strength: Synaptic weight of the connections
-    """
-    connect_dict = {
-        "rule": "fixed_indegree",
-        "indegree": indegree
-    }
-
-    synapse_dict = {
-        "weight": connection_strength
-    }
-
-    nest.Connect(src_nodes, target_nodes, conn_spec=connect_dict, syn_spec=synapse_dict)
 
 
 def main(compute_mi=False):
@@ -301,6 +129,11 @@ def main(compute_mi=False):
             stimulus_size=num_receptors,
             threshold_pot=threshold_pot
         )
+        if VERBOSITY > 1:
+            plt.imshow(reconstruction)
+            plt.imshow(image, cmap="gray")
+            plt.show()
+
         reconstruction_data.append(reconstruction.reshape(-1))
 
     # If the flag is set compute MI between the input stimulus and the reconstructed stimulus
