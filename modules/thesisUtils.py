8433c3eaf7eb54c25c417ba2749cda130ed7565e
Leo Tiez <leozeitler95@gmail.com>
2020-02-13 10:46:53 +0100

    Update: Added plotting function for connections

--- modules/thesisUtils.py
+++ modules/thesisUtils.py
@@ -3,6 +3,10 @@ import os
 import numpy as np
 from scipy.fft import idct
 from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+import nest.topology as tp
 
 
 def degree_to_rad(deg):
@@ -45,4 +49,38 @@ def load_image(name, path=None):
         path = os.getcwd() + "/test-input/"
     image = Image.open(path + name).convert("L")
     return np.asarray(image)
+
+
+def plot_connections(src_nodes, target_nodes, layer_size, save_plot=False, plot_name=None, color_mask=None):
+    plt.axis((-layer_size/2., layer_size/2., -layer_size/2., layer_size/2.))
+    source_positions = tp.GetPosition(src_nodes)
+    x_source, y_source = zip(*source_positions)
+    plt.plot(x_source, y_source, 'o')
+
+    target_positions = tp.GetPosition(target_nodes)
+    x_target, y_target = zip(*target_positions)
+    plt.plot(x_target, y_target, 'o')
+
+    for s in source_positions:
+        for t in target_positions:
+            plt.plot([s[0], t[0]], [s[1], t[1]], color="k")
+
+    if color_mask is not None:
+        for mask in color_mask:
+            area_rect = patches.Rectangle(
+                mask["lower_left"],
+                width=mask["width"],
+                height=mask["height"],
+                color=mask["color"],
+                alpha=0.2
+            )
+            plt.gca().add_patch(area_rect)
+    if plot_name is None:
+        plot_name = "connections.png"
+    if save_plot:
+        curr_dir = os.getcwd()
+        plt.savefig(curr_dir + "/figures/" + plot_name)
+    else:
+        plt.show()
+
 
