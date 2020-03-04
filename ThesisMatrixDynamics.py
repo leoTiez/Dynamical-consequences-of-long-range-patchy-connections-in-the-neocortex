f2db972e13f4411c18f41f67b4edf2bfa19419df
Leo Tiez <leozeitler95@gmail.com>
2020-03-04 17:51:23 +0100

    Update: Added expected return value for color map

--- ThesisMatrixDynamics.py
+++ ThesisMatrixDynamics.py
@@ -29,7 +29,7 @@ def main_matrix_dynamics(network_type="local_radial_lr_patchy"):
     plot_arrangement_rows = 5
     plot_arrangement_columns = 5
 
-    (_, adj_rec_sens_mat, adj_sens_sens_mat, _, _) = create_network(
+    (_, adj_rec_sens_mat, adj_sens_sens_mat, _, _, _) = create_network(
         input_stimulus,
         sens_adj_mat_needed=True,
         cap_s=cap_s,
