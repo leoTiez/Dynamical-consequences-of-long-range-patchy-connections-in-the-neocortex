5573b68071a938b9dfdd517246c4d21e3267aa15
Leo Tiez <leozeitler95@gmail.com>
2020-03-20 19:08:15 +0100

    Update: Added documentation and command line arguments for the parser

--- modules/thesisUtils.py
+++ modules/thesisUtils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import os
 import argparse
+from pathlib import Path
 import numpy as np
 from scipy.fftpack import idct
 import scipy.interpolate as ip
@@ -15,15 +16,29 @@ import nest.topology as tp
 
 
 def arg_parse():
+    """
+    Command line argument parser
+    :return: The command line arguments
+    """
     parser = argparse.ArgumentParser(description='Accept command line parameters for the reconstruction tests.')
     parser.add_argument('--agg', dest='agg', action='store_true', help='Use Agg backend for matplotlib')
     parser.add_argument('--seed', dest='seed', action='store_true', help='Seed random number generator')
+    parser.add_argument('--experiment', type=str)
+    parser.add_argument('--network', type=str)
+    parser.add_argument('--parameter', type=str)
     parsed_args = parser.parse_args()
 
     return parsed_args
 
 
 def custom_cmap(num_stimulus_discr=4, add_inh=False):
+    """
+    Custom color map for coloring the neurons and tuning map
+    :param num_stimulus_discr: Number of stimulus feature classes that can be discriminated
+    :param add_inh: Flag to determine whether inhibitory neurons are to be included as they don't have any
+    stimulus tuning
+    :return: The customised color map
+    """
     cmap = plt.get_cmap('tab10')
     new_cmap = mcolors.LinearSegmentedColormap.from_list(
         'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=0.0, b=(1/num_stimulus_discr)+0.1),
@@ -81,6 +96,7 @@ def plot_connections(
         layer_size,
         save_plot=False,
         plot_name=None,
+        save_prefix="",
         color_mask=None
 ):
     """
@@ -90,7 +106,9 @@ def plot_connections(
     :param layer_size: Size of the layer
     :param save_plot: Flag for saving the plot
     :param plot_name: Name of the saved plot file. Is not taken into account if save_plot is False
+    :param save_prefix: Naming prefix that is used if the plot save_plot is set to true
     :param color_mask: Color mask for the color/orientation map of neurons. If none it is not taken into account
+    :return None
     """
     plt.axis((-layer_size/2., layer_size/2., -layer_size/2., layer_size/2.))
     source_positions = tp.GetPosition(src_nodes)
@@ -130,13 +148,21 @@ def plot_connections(
         plot_name = "connections.png"
     if save_plot:
         curr_dir = os.getcwd()
-        plt.savefig(curr_dir + "/figures/" + plot_name)
+        Path(curr_dir + "/figures/connections/")
+        plt.savefig(curr_dir + "/figures/connections/%s_%s" % (save_prefix, plot_name))
         plt.close()
     else:
         plt.show()
 
 
 def plot_colorbar(fig, ax, num_stim_classes=4):
+    """
+    Adds a color bar to the plot
+    :param fig: The matplotlib figure
+    :param ax: The matplotlib axis
+    :param num_stim_classes: The number of stimulus feature classes that can be discriminated
+    :return: None
+    """
     step_size = 255 / float(num_stim_classes)
     bounds = [i * step_size for i in range(0, num_stim_classes + 1)]
     cmap = custom_cmap(num_stimulus_discr=num_stim_classes, add_inh=True)
@@ -153,6 +179,14 @@ def plot_colorbar(fig, ax, num_stim_classes=4):
 
 
 def perlin_noise(size_layer=50, resolution=(5, 5), spacing=0.01):
+    """
+    Creates a color map using a Perlin noise distribution
+    :param size_layer: Size of the layer
+    :param resolution: The resolution of the Perlin noise defines the mesh on which values and vectors are randomly
+    sampled
+    :param spacing: The spacing in x and y for all values that are interpolated
+    :return: The Perlin noise color map
+    """
     grid_nodes_range = np.arange(0, size_layer, spacing)
     stimulus_grid_range_x = np.linspace(0, size_layer, resolution[0])
     stimulus_grid_range_y = np.linspace(0, size_layer, resolution[1])
@@ -164,6 +198,13 @@ def perlin_noise(size_layer=50, resolution=(5, 5), spacing=0.01):
 
 
 def coordinates_to_cmap_index(layer_size, position, spacing):
+    """
+    Converts coordinates to an index in the tuning color map
+    :param layer_size: Size of the layer
+    :param position: Postion of the node
+    :param spacing: Spacing that was used for creating the cmap
+    :return: The indices of these coordinates for the color map
+    """
     position = np.asarray(position)
     if len(position.shape) <= 1:
         position = np.asarray([position])
@@ -174,16 +215,38 @@ def coordinates_to_cmap_index(layer_size, position, spacing):
 
 
 def dot_product_perlin(x_grid, y_grid, x, y, gradients):
+    """
+    The dot product step to compute the perlin noise
+    :param x_grid: The closest x value on the grid
+    :param y_grid: The closest y value on the grid
+    :param x: The x value
+    :param y: The y value
+    :param gradients: The sampled gradients
+    :return: The dot product for producing the Perlin noise
+    """
     x_weight = x - x_grid
     y_weight = y - y_grid
     return x_weight * gradients[x_grid, y_grid][0] + y_weight * gradients[x_grid, y_grid][1]
 
 
 def lerp_perlin(a, b, weight):
+    """
+    Function to linearly interpolate between a and b
+    :param a: Point a
+    :param b: Point b
+    :param weight: Weigth between 0 and 1
+    :return: Interpolation value
+    """
     return (1. - weight) * a + weight * b
 
 
 def sort_nodes_space(nodes, axis=0):
+    """
+    Sorting the nodes with respect to space and in either x or y direction
+    :param nodes: The nodes that need to be sorted
+    :param axis: Integer for the axis. 0 is x axis, 1 is y axis
+    :return: The sorted list of nodes, the sorted list of positions
+    """
     pos = tp.GetPosition(nodes)
     nodes_pos = list(zip(nodes, pos))
     if axis == 0:
@@ -195,6 +258,18 @@ def sort_nodes_space(nodes, axis=0):
 
 
 def get_in_out_degree(nodes, node_tree=None, node_pos=None, r_loc=0.5, r_p=None, size_layer=8.):
+    """
+    Computes the distribution of in- and outdegree in the network
+    :param nodes: The network nodes
+    :param node_tree: The positions of the nodes organised in a tree
+    :param node_pos: The nodes positions
+    :param r_loc: The local radius
+    :param r_p: The patchy radius. If set to None the half of the local radius is taken
+    :param size_layer: The size of the layer
+    :return: indegree dist of all connections, outdegree dist of all connections, indegree dist of local connections,
+    the outdegree dist of local connections, the indegree of long-range connections, the outdegree of long-range
+    conncetions
+    """
     in_degree = []
     out_degree = []
 
