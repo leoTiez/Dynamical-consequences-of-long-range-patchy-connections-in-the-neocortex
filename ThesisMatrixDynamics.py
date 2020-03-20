Bugfix: Fixed computation of the feedforward transformation of the input

ThesisMatrixDynamics.py
@@ -25,7 +25,17 @@ NETWORK_TYPE = {
 }
 
 
-def main_matrix_dynamics(network_type=NETWORK_TYPE["local_circ_patchy_sd"], input_type=INPUT_TYPE["plain"]):
+def main_matrix_dynamics(
+        network_type=NETWORK_TYPE["local_circ_patchy_sd"],
+        input_type=INPUT_TYPE["plain"]
+):
+    """
+    Calculate the network dynamics based on matrix calculations. For that, the input is once transformed via the
+    feedforward matrix and then the dynamics are investigated via the weight matrix for the recurrent connections
+    :param network_type: Network type. This is an integer number defined in the NETWORK_TYPE dictionary
+    :param input_type: Input type. This is an integer number defined in INPUT_TYPE dictionary
+    :return: None
+    """
     # load input stimulus
     stimulus_size = (50, 50)
     input_stimulus = stimulus_factory(input_type, size=stimulus_size)
@@ -44,11 +54,10 @@ def main_matrix_dynamics(network_type=NETWORK_TYPE["local_circ_patchy_sd"], inpu
     sens_weight_mat = network.get_sensory_weight_mat()
 
     # Sufficient to use only 255 as we don't use the neurons themselves
-    input_matrix = np.full(stimulus_size, 255)
-    sensory_activity = network.adj_rec_sens_mat.T.dot(input_matrix.reshape(-1))
-    fig, axes = plt.subplots(plot_arrangement_rows, plot_arrangement_columns)
-    fig.set_figheight(10)
-    fig.set_figwidth(20)
+    input_matrix = np.ones(stimulus_size[0] * stimulus_size[1] + 1)
+    input_matrix[:stimulus_size[0] * stimulus_size[1]] = 255
+    sensory_activity = input_matrix.reshape(-1).dot(network.ff_weight_mat)[:-1]
+    fig, axes = plt.subplots(plot_arrangement_rows, plot_arrangement_columns, figsize=(20, 10))
     for ax in axes.reshape(-1):
         ax.imshow(sensory_activity.reshape(network_shape))
         sensory_activity = sens_weight_mat.T.dot(sensory_activity)