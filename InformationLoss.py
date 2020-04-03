41a0f3117cc4d49df8f6e8098af7133e51cef96c
Leo Tiez <leozeitler95@gmail.com>
2020-04-03 18:47:28 +0200

    Update: Make plotting more flexible by accepting more cmd pararmeters

--- InformationLoss.py
+++ InformationLoss.py
@@ -6,6 +6,7 @@ from scipy.stats import norm
 from createThesisNetwork import NETWORK_TYPE
 from modules.networkConstruction import TUNING_FUNCTION
 from modules.thesisUtils import arg_parse_plts
+from modules.createStimulus import INPUT_TYPE
 
 PLOT_TYPE={
     "bar": 0,
@@ -22,7 +23,7 @@ def main_error(
         path_part,
         network="",
         stimulus="",
-        attribute="",
+        attributes=[""],
         plt_bottom_margin=0.2,
         plot_type=PLOT_TYPE["bar"],
         save_plot=False
@@ -45,15 +46,18 @@ def main_error(
 
         split_full = file_full.split("_")
         split_part = file_part.split("_")
-        is_attribute = attribute in file_full and attribute in file_part
+        is_attribute = np.all(np.asarray([a in file_full and a in file_part for a in attributes]))
         is_patchy = "patchy" in file_full and "patchy" in file_part
         if "patchy" in network:
             is_network = (network in file_full and network in file_part) and is_patchy
         elif "random" in network:
             is_network = network in split_full[1] and network in split_part[1]
+        elif len(network) == 0:
+            is_network = True
         else:
             is_network = (network in file_full and network in file_part) and not is_patchy
-        is_stimulus = stimulus in file_full and stimulus in file_part
+
+        is_stimulus = stimulus in split_full[-6] and stimulus in split_part[-9]
         if is_attribute and is_network and is_stimulus:
             path_full = path_full + "/"
             path_part = path_part + "/"
@@ -68,14 +72,17 @@ def main_error(
                 label = file_full\
                     .replace(network, "", 1)\
                     .replace(stimulus, "")\
-                    .replace(attribute, "")\
                     .replace("function", "")\
                     .replace("tuning", "")\
                     .replace("img_prop", "")\
                     .replace("mean", "")\
                     .replace("error", "")\
+                    .replace("0.8", "")\
                     .replace(".txt", "")\
                     .replace("_", " ")
+                for a in attributes:
+                    label = label.replace(a, "")
+
                 label_names.append(label)
                 mean_full_f = open(curr_dir + path_full + file_full, "r")
                 mean_part_f = open(curr_dir + path_part + file_part, "r")
@@ -89,6 +96,7 @@ def main_error(
 
     if plot_type == PLOT_TYPE["bar"]:
         x_axis = np.arange(0, len(label_names))
+        plt.figure(figsize=(10, 8))
         plt.bar(x_axis + 0.0, mean_full_l, label="Error Full", width=0.25)
         plt.bar(x_axis + 0.25, mean_part_l, label="Error Part", width=0.25)
         plt.bar(x_axis + 0.5, inf_loss_l, label="Lost Information", width=0.25)
@@ -102,7 +110,7 @@ def main_error(
             plt.show()
         else:
             curr_dir = os.getcwd()
-            save_name = curr_dir + "/figures/lost-information/%s_network_%s_80.png" % (network, attribute)
+            save_name = curr_dir + "/figures/lost-information/%s_network_%s_80.png" % (network, attributes[0])
             plt.savefig(save_name)
             plt.close()
 
@@ -125,7 +133,7 @@ def main_error(
             plt.show()
         else:
             curr_dir = os.getcwd()
-            save_name = curr_dir + "/figures/lost-information/gauss_dist_%s_network_%s_80.png" % (network, attribute)
+            save_name = curr_dir + "/figures/lost-information/gauss_dist_%s_network_%s_80.png" % (network, attributes[0])
             plt.savefig(save_name)
             plt.close()
 
@@ -137,7 +145,9 @@ if __name__ == '__main__':
     save_plot = True
     path_full = "experiments/error-full/error"
     path_part = "experiments/error-80-net/error"
-
+    networks = [""]
+    tunings = [""]
+    stimuli = [""]
     if cmd_par.type is not None:
         plt_type = PLOT_TYPE[cmd_par.type]
 
@@ -150,16 +160,42 @@ if __name__ == '__main__':
     if cmd_par.path_part is not None:
         path_part = cmd_par.path_part
 
-    for network in NETWORK_TYPE.keys():
-        for tuning in TUNING_FUNCTION.keys():
-            main_error(
-                path_full="experiments/error-full/error",
-                path_part="experiments/error-80-net/error",
-                network=network,
-                attribute=tuning,
-                plt_bottom_margin=0.2,
-                plot_type=plt_type,
-                save_plot=save_plot
-            )
+    if cmd_par.network is not None:
+        if cmd_par.network.lower() == "all":
+            networks = list(NETWORK_TYPE.keys())
+        elif cmd_par.network.lower() in list(NETWORK_TYPE.keys()):
+            networks[0] = cmd_par.network.lower()
+        else:
+            raise ValueError("Not a valid network type")
+
+    if cmd_par.tuning is not None:
+        if cmd_par.tuning.lower() == "all":
+            tunings = list(TUNING_FUNCTION.keys())
+        elif cmd_par.tuning.lower() in list(TUNING_FUNCTION.keys()):
+            tunings[0] = cmd_par.tuning.lower()
+        else:
+            raise ValueError("Not a valid tuning function")
+
+    if cmd_par.input is not None:
+        if cmd_par.input.lower() == "all":
+            stimuli = list(INPUT_TYPE.keys())
+        elif cmd_par.input.lower() in list(INPUT_TYPE.keys()):
+            stimuli[0] = cmd_par.input.lower()
+        else:
+            raise ValueError("Not a valid stimulus type")
+
+    for network in networks:
+        for tuning in tunings:
+            for stimulus in stimuli:
+                main_error(
+                    path_full=path_full,
+                    path_part=path_part,
+                    network=network,
+                    stimulus=stimulus,
+                    attributes=[tuning],
+                    plt_bottom_margin=0.3,
+                    plot_type=plt_type,
+                    save_plot=save_plot
+                )
 
 
