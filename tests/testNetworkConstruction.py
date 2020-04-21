ae2ee8e8404ec2bcddd7336a4b704b5b664c750c
Leo Tiez <leozeitler95@gmail.com>
2020-04-21 12:36:21 +0200

    Bugfix: Updated and fixed existing tests

--- tests/testNetworkConstruction.py
+++ tests/testNetworkConstruction.py
@@ -4,7 +4,9 @@ import unittest
 import modules.networkConstruction as nc
 import modules.createStimulus as cs
 import modules.thesisUtils as tu
+import modules.stimulusReconstruction as sr
 import numpy as np
+from scipy.spatial import KDTree
 
 import nest.topology as tp
 import nest
@@ -38,14 +40,24 @@ class NetworkConstructionTest(unittest.TestCase):
             size_layer=self.size_layer
         )
         self.torus_nodes = nest.GetNodes(self.torus_layer)[0]
+        self.inh_nodes = np.random.choice(np.asarray(self.torus_nodes), self.num_neurons // 5, replace=False)
         self.min_id_torus = min(self.torus_nodes)
+        self.torus_positions = tp.GetPosition(self.torus_nodes)
+        self.torus_tree = KDTree(self.torus_positions)
+
+        self.perlin_resolution = (15, 15)
+        self.perlin_spacing = 0.1
 
         (self.tuning_to_neuron_map,
          self.neuron_to_tuning_map,
-         self.tuning_weight_vector,
-         _) = nc.create_stimulus_tuning_map(
+         self.tuning_weight_vector) = nc.create_perlin_stimulus_map(
             self.torus_layer,
-            num_stimulus_discr=self.num_stimulus_discr
+            self.inh_nodes,
+            num_stimulus_discr=self.num_stimulus_discr,
+            resolution=self.perlin_resolution,
+            spacing=self.perlin_spacing,
+            plot=False,
+            save_plot=False
         )
 
         self.retina = nc.create_input_current_generator(
@@ -57,39 +69,15 @@ class NetworkConstructionTest(unittest.TestCase):
 
     def reset(self):
         nest.ResetKernel()
-        self.torus_layer, self.spike_det, self.multi = nc.create_torus_layer_uniform(
-            num_neurons=self.num_neurons,
-            neuron_type=self.neuron_type,
-            rest_pot=self.rest_pot,
-            threshold_pot=self.threshold_pot,
-            time_const=self.time_const,
-            capacitance=self.capacitance,
-            size_layer=self.size_layer
-        )
-
-        self.torus_nodes = nest.GetNodes(self.torus_layer)[0]
-        self.min_id_torus = min(self.torus_nodes)
-
-        (self.tuning_to_neuron_map,
-         self.neuron_to_tuning_map,
-         self.tuning_weight_vector,
-         _) = nc.create_stimulus_tuning_map(
-            self.torus_layer,
-            num_stimulus_discr=self.num_stimulus_discr
-        )
-
-        self.retina = nc.create_input_current_generator(
-            self.input_stimulus,
-            organise_on_grid=self.organise_on_grid
-        )
-
-        self.receptors = nest.GetNodes(self.retina)[0]
+        self.setUp()
 
     # ################################################################################################################
     # Functions used for thesis simulations
     # ################################################################################################################
 
     def test_create_distinct_sublayer_boxes(self):
+        self.reset()
+
         size_boxes = 4.
         size_layer = 8.
         expected_anchors = np.asarray([[-2.0, -2.0], [2.0, -2.0], [-2.0, 2.0], [2.0, 2.0]])
@@ -109,6 +97,8 @@ class NetworkConstructionTest(unittest.TestCase):
         )
 
     def test_create_torus_layer_uniform(self):
+        self.reset()
+
         # Create simple layer
         num_neurons = 3200
         neuron_type = "iaf_psc_delta"
@@ -203,21 +193,42 @@ class NetworkConstructionTest(unittest.TestCase):
         self.assertNotEqual(len(multi_connect), 0, "Connection between multimeter and neurons doesn't exist")
 
     def test_create_local_circular_connections(self):
-        r_loc = 0.3
-        p_loc = 0.4
-
         self.reset()
 
-        nc.create_local_circular_connections(self.torus_layer, r_loc=r_loc, p_loc=p_loc)
+        r_loc = 0.3
+        inh_weight = -5.
+        cap_s = 2.
+        connect_dict = {
+            "rule": "pairwise_bernoulli",
+            "p": 0.4,
+        }
+
+        nc.create_local_circular_connections(
+            self.torus_layer,
+            self.torus_tree,
+            self.inh_nodes,
+            inh_weight=inh_weight,
+            cap_s=cap_s,
+            connect_dict=connect_dict,
+            r_loc=r_loc,
+        )
+
         connect = nest.GetConnections(self.torus_nodes)
         for c in connect:
             s = nest.GetStatus([c], "source")[0]
             t = nest.GetStatus([c], "target")[0]
             if t in self.torus_nodes:
                 d = tp.Distance([s], [t])[0]
+                w = nest.GetStatus([c], "weight")[0]
                 self.assertLessEqual(d, r_loc)
+                if s in self.inh_nodes:
+                    self.assertEqual(w, inh_weight, "Inhibitory weight was not set properly")
+                else:
+                    self.assertEqual(w, cap_s, "Excitatory weight was not set properly")
 
     def test_create_stimulus_tuning_map(self):
+        self.reset()
+
         num_stimulus_discr = 4
         tuning_to_neuron_map, neuron_to_tuning_map, tuning_weight_vector, _ = nc.create_stimulus_tuning_map(
             self.torus_layer,
@@ -242,16 +253,21 @@ class NetworkConstructionTest(unittest.TestCase):
                 )
 
     def test_create_stimulus_based_local_connections(self):
+        self.reset()
+
         r_loc = 0.2
         connect_dict = {"rule": "pairwise_bernoulli", "p": 0.8}
-
-        self.reset()
+        inh_weight = -5.
+        cap_s = 2.
 
         nc.create_stimulus_based_local_connections(
             self.torus_layer,
+            self.torus_tree,
             self.neuron_to_tuning_map,
             self.tuning_to_neuron_map,
+            self.inh_nodes,
             r_loc=r_loc,
+            inh_weight=inh_weight,
             connect_dict=connect_dict
         )
 
@@ -259,26 +275,39 @@ class NetworkConstructionTest(unittest.TestCase):
         for c in connect:
             s = nest.GetStatus(c, "source")
             t = nest.GetStatus(c, "target")
+            w = nest.GetStatus([c], "weight")[0]
+
             self.assertLessEqual(tp.Distance(s, t), r_loc)
             self.assertEqual(self.neuron_to_tuning_map[s], self.neuron_to_tuning_map[t])
+            if s in self.inh_nodes:
+                self.assertEqual(w, inh_weight, "Inhibitory weight was not set properly")
+            else:
+                self.assertEqual(w, cap_s, "Excitatory weight was not set properly")
 
     def test_create_stimulus_based_patches_random(self):
+        self.reset()
+
         num_patches = 2
         r_loc = 0.2
         p_loc = 0.1
-        # With connection dict
+        p_p = 0.4
         connect_dict = {"rule": "pairwise_bernoulli", "p": 0.7}
-
-        self.reset()
+        cap_s = 2.
+        filter_patches = True
 
         nc.create_stimulus_based_patches_random(
             self.torus_layer,
             self.neuron_to_tuning_map,
             self.tuning_to_neuron_map,
+            self.inh_nodes,
+            self.torus_tree,
             num_patches=num_patches,
             r_loc=r_loc,
             p_loc=p_loc,
+            p_p=p_p,
+            cap_s=cap_s,
             connect_dict=connect_dict,
+            filter_patches=filter_patches
         )
 
         connect = nest.GetConnections(self.torus_nodes)
@@ -287,11 +316,16 @@ class NetworkConstructionTest(unittest.TestCase):
             t = nest.GetStatus([c], "target")[0]
             if t in self.torus_nodes:
                 d = tp.Distance([s], [t])[0]
+                w = nest.GetStatus([c], "weight")[0]
+
                 self.assertLessEqual(d, self.size_layer)
                 self.assertGreaterEqual(d, r_loc)
                 self.assertEqual(self.neuron_to_tuning_map[s], self.neuron_to_tuning_map[t])
+                self.assertEqual(w, cap_s, "The weight for patchy connections wasn't set properly")
 
     def test_create_input_current_generator(self):
+        self.reset()
+
         input_stimulus = cs.image_with_spatial_correlation(size_img=(50, 50), radius=3, num_circles=80)
         organise_on_grid = True
         retina = nc.create_input_current_generator(
@@ -302,16 +336,18 @@ class NetworkConstructionTest(unittest.TestCase):
         receptors = nest.GetNodes(retina)[0]
         amplitude = nest.GetStatus(receptors, "amplitude")
         self.assertTrue(
-            np.all(np.asarray(amplitude) == input_stimulus.reshape(-1) * 1e12),
+            np.all(np.asarray(amplitude) == input_stimulus.reshape(-1)),
             "The input stimulus and the current in the retina don't match"
         )
 
     def test_create_connections_rf(self):
-        rf_size = (10, 10)
-        connect_dict = {"rule": "pairwise_bernoulli", "p": 1.}
-        synaptic_strength = 1.
-
         self.reset()
+
+        rf_size = (10, 10)
+        synaptic_strength = 1.
+        calc_error = True
+        use_dc = False
+        p_rf = 1.
 
         positions = tp.GetPosition(self.torus_nodes)
         rf_centers = [
@@ -323,65 +359,39 @@ class NetworkConstructionTest(unittest.TestCase):
         ]
         rf_mask = {"lower_left": [-rf_size[0]/2., -rf_size[1]/2.], "upper_right": [rf_size[0]/2., rf_size[1]/2.]}
 
-        adj_mat = nc.create_connections_rf(
-            self.retina,
-            self.torus_layer,
+        adj_mat, recon = nc.create_connections_rf(
+            self.input_stimulus,
+            self.torus_nodes,
             rf_centers,
             self.neuron_to_tuning_map,
+            self.inh_nodes,
             rf_size=rf_size,
-            connect_dict=connect_dict,
-            synaptic_strength=synaptic_strength
+            p_rf=p_rf,
+            synaptic_strength=synaptic_strength,
+            total_num_target=self.num_neurons,
+            target_layer_size=self.size_layer,
+            calc_error=calc_error,
+            use_dc=use_dc,
         )
 
         # Check for lower equal since there can be less neurons at the edges of the retina
         self.assertLessEqual(
-            np.max(adj_mat.sum(axis=0)),
+            np.max((adj_mat > 0).sum(axis=0)),  # (adj_mat > 0) since it is technically no adj mat but a tranform matrix
             rf_size[0] * rf_size[1],
             "There are receptors to which no connections are established although they are in the receptive field"
         )
 
-        tuning_disc_step = 255 / float(self.num_stimulus_discr)
-        for n in self.torus_nodes:
-            n_position = tp.GetPosition([n])[0]
-            rf_center = ((n_position[0] / (self.size_layer / 2.)) * self.input_stimulus.shape[1] / 2.,
-                         (n_position[1] / (self.size_layer / 2.)) * self.input_stimulus.shape[0] / 2.)
-            rf = tp.SelectNodesByMask(self.retina, rf_center, tp.CreateMask("rectangular", specs=rf_mask))
+        img_vector = np.ones(self.input_stimulus.size + 1)
+        img_vector[:-1] = self.input_stimulus.reshape(-1)
+        recon_transform = sr.direct_stimulus_reconstruction(img_vector.dot(adj_mat)[:-1], adj_mat)
 
-            connect = nest.GetConnections(source=self.receptors, target=[n])
-            source = nest.GetStatus(connect, "source")
-
-            for s, c in zip(source, connect):
-                self.assertIn(s, rf, "Receptor not in the receptive field")
-                pos = tp.GetPosition([s])[0]
-                grid_pos_x = int(self.input_stimulus.shape[0] / 2. + np.floor(pos[0]))
-                grid_pos_y = int(self.input_stimulus.shape[1] / 2. - np.ceil(pos[1]))
-                self.assertEqual(
-                    tp.GetElement(self.retina, (grid_pos_x, grid_pos_y))[0],
-                    s,
-                    "Did not compute the correct grid position"
-                )
-
-                stimulus = self.input_stimulus[grid_pos_x, grid_pos_y]
-                weight = nest.GetStatus([c], "weight")[0]
-                stimulus_tuning = self.neuron_to_tuning_map[n]
-                reacts_on_stimulus = stimulus_tuning * tuning_disc_step <= stimulus <\
-                                     (stimulus_tuning + 1) * tuning_disc_step
-
-                if reacts_on_stimulus:
-                    self.assertEqual(
-                        synaptic_strength,
-                        weight,
-                        "Synaptic weight is not set to the correct value. Actual value %s, expected value %s"
-                        % (weight, synaptic_strength)
-                    )
-                else:
-                    self.assertEqual(
-                        weight,
-                        0,
-                        "Synaptic weight is not set to zero although not correct stimulus. Actual value %s" % weight
-                    )
+        for res, exp in zip(recon.reshape(-1), recon_transform.reshape(-1)):
+            self.assertAlmostEqual(res, exp, msg="The transformation through the feedforward matrix does not lead to a "
+                                                 "similar reconstruction")
 
     def test_create_torus_layer_with_jitter(self):
+        self.reset()
+
         # Must be number that has a square root in N
         num_neurons = 144
         jitter = 0.01
@@ -414,11 +424,11 @@ class NetworkConstructionTest(unittest.TestCase):
             self.assertLessEqual(pos[1], np.abs(grid[0][1] + jitter), "Distance in y to original position too high")
 
     def test_create_distant_np_connections(self):
+        self.reset()
+
         r_loc = .2
         p_loc = .5
         p_p = 0.5
-
-        self.reset()
 
         nc.create_distant_np_connections(self.torus_layer, p_loc=p_loc, r_loc=r_loc, p_p=p_p)
 
@@ -432,15 +442,25 @@ class NetworkConstructionTest(unittest.TestCase):
             self.assertGreaterEqual(d, r_loc, "Distance is too low")
 
     def test_create_random_patches(self):
+        self.reset()
+
         r_loc = 0.2
         p_loc = 0.8
         p_p = 0.6
         num_patches = 2
         d_p = r_loc
+        cap_s = 1.
 
-        self.reset()
-
-        nc.create_random_patches(self.torus_layer, r_loc=r_loc, p_loc=p_loc, num_patches=num_patches, p_p=p_p)
+        nc.create_random_patches(
+            self.torus_layer,
+            self.inh_nodes,
+            r_loc=r_loc,
+            p_loc=p_loc,
+            num_patches=num_patches,
+            p_p=p_p,
+            cap_s=cap_s,
+            plot=False
+        )
 
         for s in self.torus_nodes:
             conn = nest.GetConnections(source=[s])
@@ -468,13 +488,14 @@ class NetworkConstructionTest(unittest.TestCase):
                 self.assertLessEqual(len(patches), num_patches, "Created too many patches")
 
     def test_create_overlapping_patches(self):
+        self.reset()
+
         r_loc = 0.3
         p_loc = 0.5
         p_r = r_loc / 2.
         distance = .7
         num_patches = 2
         p_p = 0.3
-        self.reset()
 
         nc.create_overlapping_patches(
             self.torus_layer,
@@ -503,6 +524,8 @@ class NetworkConstructionTest(unittest.TestCase):
                 )
 
     def test_create_shared_patches(self):
+        self.reset()
+
         r_loc = 0.1
         p_loc = 0.9
         p_p = 0.3
@@ -510,8 +533,6 @@ class NetworkConstructionTest(unittest.TestCase):
         size_boxes = 0.2
         num_patches = 2
         num_shared_patches = 3
-
-        self.reset()
 
         nc.create_shared_patches(
             self.torus_layer,
@@ -570,6 +591,8 @@ class NetworkConstructionTest(unittest.TestCase):
                     self.assertLessEqual(len(box_patches), num_shared_patches + 1, "Created too many patches per box")
 
     def test_create_partially_overlapping_patches(self):
+        self.reset()
+
         r_loc = 0.1
         p_loc = 0.7
         d_p = r_loc
@@ -578,8 +601,6 @@ class NetworkConstructionTest(unittest.TestCase):
         num_shared_patches = 3
         num_patches_replaced = 3
         p_p = 0.3
-
-        self.reset()
 
         nc.create_partially_overlapping_patches(
             self.torus_layer,
@@ -643,8 +664,9 @@ class NetworkConstructionTest(unittest.TestCase):
         plot = False
         save_plot = False
 
-        tuning_to_neuron, neuron_to_tuning, weight_vector, color_map = nc.create_perlin_stimulus_map(
+        tuning_to_neuron, neuron_to_tuning, color_map = nc.create_perlin_stimulus_map(
             self.torus_layer,
+            self.inh_nodes,
             num_stimulus_discr=num_stimulus_discr,
             resolution=resolution,
             spacing=spacing,
@@ -653,19 +675,15 @@ class NetworkConstructionTest(unittest.TestCase):
         )
 
         for n in self.torus_nodes:
-            self.assertIn(n, tuning_to_neuron[neuron_to_tuning[n]], "Neuron is not assigned to the correct tuning list")
-            self.assertEqual(
-                weight_vector[n - self.min_id_torus],
-                (neuron_to_tuning[n] + 1) / num_stimulus_discr,
-                "Did not set the correct stimulus weight"
-            )
-            p = tp.GetPosition([n])[0]
-            x_grid, y_grid = tu.coordinates_to_cmap_index(self.size_layer, p, spacing)
-            self.assertEqual(
-                color_map[x_grid, y_grid],
-                neuron_to_tuning[n],
-                "Color map and tuning preference doesn't match"
-            )
+            if n not in self.torus_nodes:
+                self.assertIn(n, tuning_to_neuron[neuron_to_tuning[n]], "Neuron is not assigned to the correct tuning list")
+                p = tp.GetPosition([n])[0]
+                x_grid, y_grid = tu.coordinates_to_cmap_index(self.size_layer, p, spacing)
+                self.assertEqual(
+                    color_map[x_grid, y_grid],
+                    neuron_to_tuning[n],
+                    "Color map and tuning preference doesn't match"
+                )
 
 
 if __name__ == '__main__':
