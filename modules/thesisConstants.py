118b68a3d3135f4c1bd0b342fbf28879a8c2b40d
Leo Tiez <leozeitler95@gmail.com>
2020-05-01 18:53:04 +0200

    Update: Use single module with all constants used in the project

---
+++ modules/thesisConstants.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+import numpy as np
+
+GLOBAL_CONNECTIVITY = 0.0123
+R_MAX = 8.
+
+PERLIN_INPUT = [4, 8, 16, 50]
+FF_FACTORS_PAR = [0.2, 0.8, 1.2, 1.8]
+PATCHES_PAR = np.arange(1, 10, 1)
+FUNC_MAP_CLUSTER_PAR = [(4, 4), (8, 8), (12, 12), (16, 16), (20, 20)]
+ALPHA_PAR = np.arange(0.5, 1.0, 0.1)
+
+NETWORK_TYPE = {
+    "random": 0,
+    "local_circ": 1,
+    "local_sd": 2,
+    "local_circ_patchy_sd": 3,
+    "local_circ_patchy_random": 4,
+    "local_sd_patchy_sd": 5,
+    "input_only": 6
+}
+
+INPUT_TYPE = {
+    "plain": 0,
+    "perlin": 1,
+    "bar": 2,
+    "circles": 3,
+    "natural": 4,
+    "edges": 5,
+    "random": 6
+}
+
+PARAMETER_DICT = {
+    "tuning": 0,
+    "cluster": 1,
+    "patches": 2,
+    "weights": 3,
+    "alpha": 4
+}
+
+TUNING_FUNCTION = {
+    "step": 0,
+    "gauss": 1,
+    "linear": 2
+}
+
