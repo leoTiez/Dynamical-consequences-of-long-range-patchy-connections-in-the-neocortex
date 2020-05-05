Bugfix: define the correct scaling weights and divisor

modules/stimulusReconstruction.py
@@ -117,14 +117,21 @@ def direct_stimulus_reconstruction(
 def oblivious_stimulus_reconstruction(
         firing_rates,
         ff_adj_matrix,
-        tuning_vector
+        tuning_vector,
+        fr_min=2.,
 ):
-    class_transformation = (tuning_vector) / (tuning_vector.max())
+    fr = np.zeros(firing_rates.size)
+    if fr_min is None:
+        fr_min = np.mean(firing_rates)
+
+    threshold_mask = firing_rates > fr_min
+    fr[threshold_mask] = firing_rates[threshold_mask]
+    class_transformation = (tuning_vector + 1) / (tuning_vector.max() + 1)
     # Set no transformation for inhibitory neurons
     class_transformation[class_transformation <= 0] = 1.
-    feat_trans_fr = firing_rates * class_transformation
+    feat_trans_fr = fr * class_transformation
     reconstruction = ff_adj_matrix.dot(feat_trans_fr)
-    reconstruction /= ff_adj_matrix.sum()
+    reconstruction = np.nan_to_num(reconstruction / ff_adj_matrix.dot(fr))
     return reconstruction.reshape(int(np.sqrt(reconstruction.size)), int(np.sqrt(reconstruction.size)))
 
 